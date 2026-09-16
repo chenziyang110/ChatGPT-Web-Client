5bde500de675355a74fa7564831ca34a96c014f0
Robin11 <43693913+chenziyang110@users.noreply.github.com>
2026-09-16 11:52:10 +0800

    feat: add electron main process

---
+++ src/main/main.ts
@@ -0,0 +1,10 @@
+import { app } from 'electron';
+import { createWindow } from './window';
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
